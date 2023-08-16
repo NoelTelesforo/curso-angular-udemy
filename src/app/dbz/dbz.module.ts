dfa0bf0272f4460be4f19679b7a5794d25785e68
Noel Telesforo <ntelesforomartinez@gmail.com>
2023-08-16 15:29:03 -0600

    Componente add-chracter

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+
+
 
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    ListComponent
+    ListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
