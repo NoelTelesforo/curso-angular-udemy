108e178aa92f21740b0b4d2eccc7c161b9cc8f49
Noel Telesforo <ntelesforomartinez@gmail.com>
2023-08-16 19:10:46 -0600

    Forms Module, agrega un personaje

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Character } from '../components/interfaces/character.interface';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
