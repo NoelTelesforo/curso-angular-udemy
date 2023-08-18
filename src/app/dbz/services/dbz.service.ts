0afbf3a1d2efba145cc593ff06596eeb7e4296f8
Noel Telesforo <ntelesforomartinez@gmail.com>
2023-08-17 21:30:19 -0600

    Id de los personajes

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({providedIn: 'root'})
 export class DbzService {
    // Arreglo de personajes
    characters: Character[] = [{
+    id: uuid(),
     name: 'Krillin',
     power: 1000
   },
   {
+    id: uuid(),
     name: 'Goku',
     power: 9500
   },
   {
+    id: uuid(),
     name: 'Vegeta',
     power: 7500
   }];
 
   // Agrega un personaje al listado
   onNewCharacter(character: Character): void {
-    this.characters.push(character);
+
+    const newCharacter: Character = {id: uuid(), ...character};
+
+    this.characters.push(newCharacter);
   }
 
   // Elimina un personaje de la lista
